2f024cd413093aaaebaac05dc342233891295c5e
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-07-14 20:32:49 -0500

    added entity and repo folders

--- src/main/java/com/pluralsight/FundamentalsApplication.java
+++ src/main/java/com/pluralsight/FundamentalsApplication.java
@@ -1,4 +1,4 @@
-package com.pluralsight.fundamentals;
+package com.pluralsight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
