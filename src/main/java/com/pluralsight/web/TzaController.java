693ffef561d31240b766ca88c581ed570b0e4581
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-07-25 16:25:30 -0500

    added dependency for thymeleaf

--- src/main/java/com/pluralsight/web/TzaController.java
+++ src/main/java/com/pluralsight/web/TzaController.java
@@ -1,4 +1,15 @@
 package com.pluralsight.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TzaController {
+
+    @GetMapping("/applications")
+    public String retrieveApplication(Model model){
+        model.addAttribute("applications", applicationService.listApplications());
+        return "applications";
+    }
 }
